e9b974d17a849499b0103357c3763905cf3d7712
Tobias Latta <tobiaslatta@7090GLOBAL.fritz.box>
2023-04-09 15:07:59 +0200

    split select device and play album / add selected_device json / add posiblitity to post deviceId using flask

---
+++ getDevice.py
@@ -0,0 +1,17 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/select-device', methods=['POST'])
+def select_device():
+    data = request.get_json()
+
+    # Save selected device data to selected_device.json
+    with open('selected_device.json', 'w') as f:
+        json.dump(data, f)
+
+    return jsonify({'message': 'Device selected'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug=True)
--- playAlbum.py
+++ playAlbum.py
@@ -2,26 +2,11 @@ import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-with open('devices.json') as f:
-    devices_data = json.load(f)
+# Get the device ID from selected_device.json
+with open('selected_device.json') as f:
+    selected_device_data = json.load(f)
 
-devices = devices_data['devices']
-
-#for device in devices:
-    #print("Device name:", device['name'], device['id'])
-
-# Select a device by id
-device_id = "18f64e611c1eebf8885594a4b4e04d30eacfbbf8"  # mac
-device = next((d for d in devices if d['id'] == device_id), None)
-
-# Print the device information
-if device:
-    print("Device found")
-    print("Device name:", device['name'])
-    print("Device type:", device['type'])
-    print("Device volume:", device['volume_percent'])
-else:
-    print("Device not found")
+device_id = selected_device_data['device_id']
 
 # Set up Spotify authentication
 with open('secrets.json', 'r') as f:
@@ -35,5 +20,3 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets['client_id'],
 # Play the first song of an album
 album_uri = 'spotify:album:0S0KGZnfBGSIssfF54WSJh'  # Change this to the URI of the album you want to play
 sp.start_playback(device_id=device_id, context_uri=album_uri, offset={"position": 0})
-
-
